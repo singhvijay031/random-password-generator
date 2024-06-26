2e7b694cb2b19682511dc1f76cd623ad5a1d805b
Bijay <bijaymahato031@gmail.com>
2024-06-26 23:22:16 +0545

    inital commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const nums = "0123456789";
+const symbols = "!@#$%^&*_-+?";
+
+function generatePassword(length) {
+  const combinedChars = upperCase + lowerCase + nums + symbols;
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * combinedChars.length);
+    password += combinedChars[randomIndex];
+  }
+  return password;
+}
+
+const passwordLength = 7;
+const randomPassword = generatePassword(passwordLength);
+console.log(`Generated Password: ${randomPassword}`);
