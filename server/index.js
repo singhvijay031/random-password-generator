58c380bae67b39357e8e6b9f08fc4f268067a861
Bijay <bijaymahato031@gmail.com>
2024-06-29 03:18:20 +0545

    port changed

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 8000;
 
 app.use(express.json());
 
 app.post("/api/generate-password", require("./api/generate-password"));
 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
