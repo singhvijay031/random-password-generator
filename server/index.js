56f2ef7e407e92449bdb41f458fbf2ad13ba58e7
Bijay <bijaymahato031@gmail.com>
2024-06-29 03:34:16 +0545

    cors error fixed

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
+// backend/index.js
 const express = require("express");
-const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 8000;
+const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.post("/api/generate-password", require("./api/generate-password"));
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
