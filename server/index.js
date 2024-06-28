055a26989032de68deac9837856715fbc8c8a94a
Bijay <bijaymahato031@gmail.com>
2024-06-29 00:34:27 +0545

    index.js modified

--- server/index.js
+++ server/index.js
@@ -1,19 +1,41 @@
-const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowerCase = "abcdefghijklmnopqrstuvwxyz";
-const nums = "0123456789";
-const symbols = "!@#$%^&*_-+?/|<>";
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = 8000;
 
-function generatePassword(length) {
-  const combinedChars = upperCase + lowerCase + nums + symbols;
+app.use(cors());
+app.use(express.json());
+
+const generatePassword = (length, options) => {
+  const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numberChars = "0123456789";
+  const symbolChars = "!@#$%^&*_-+?/|<>";
+  const spaceChar = " ";
+
+  let characters = "";
+  if (options.lowercase) characters += lowercaseChars;
+  if (options.uppercase) characters += uppercaseChars;
+  if (options.numbers) characters += numberChars;
+  if (options.symbols) characters += symbolChars;
+  if (options.spaces) characters += spaceChar;
+
   let password = "";
-
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * combinedChars.length);
-    password += combinedChars[randomIndex];
+  while (password.length < length) {
+    let char = characters.charAt(Math.floor(Math.random() * characters.length));
+    if (options.excludeDuplicate && password.includes(char)) continue;
+    password += char;
   }
-  return password;
-}
 
-const passwordLength = 12;
-const randomPassword = generatePassword(passwordLength);
-console.log(`Generated Password: ${randomPassword}`);
+  return password;
+};
+
+app.post("/generate-password", (req, res) => {
+  const { length, options } = req.body;
+  const password = generatePassword(length, options);
+  res.json({ password });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is Running at http://localhost:${PORT}`);
+});
